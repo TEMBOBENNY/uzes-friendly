bee80130830caee88ebbd953ece4d1d964ceb4fb
TEMBOBENNY <tembobenny49@gmail.com>
2026-06-30 13:20:48 +0200

    Remove firebase-app-check-playintegrity - no valid version exists for sideloaded APK, add back when on Play Store

--- android/app/src/main/java/com/uzes/app/MainActivity.java
+++ android/app/src/main/java/com/uzes/app/MainActivity.java
@@ -1,17 +1,5 @@
 package com.uzes.app;
 
-import android.os.Bundle;
-import com.google.firebase.appcheck.FirebaseAppCheck;
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Initialize Firebase App Check with Play Integrity
-        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
-            PlayIntegrityAppCheckProviderFactory.getInstance()
-        );
-    }
-}
+public class MainActivity extends BridgeActivity {}
